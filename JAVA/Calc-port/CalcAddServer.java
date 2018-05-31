3f0f797daf8b449664ddc8da143a536e94d3f5b1
lowelll <yejin2651@naver.com>
2018-05-31 14:50:32 +0900

    각 기능 메소드로 묶음
    process()에 있던 코드를 기능 별 메소드로 묶음.

--- JAVA/Calc-port/CalcAddServer.java
+++ JAVA/Calc-port/CalcAddServer.java
@@ -4,6 +4,11 @@ import java.net.*;
 class CalcAddServer 
 {
 	private ServerSocket server = null;
+	private Socket socket = null;
+
+	private BufferedWriter bw = null;
+	private	BufferedReader br = null;
+	private String exp;
 
 	CalcAddServer () {
 		try {
@@ -13,40 +18,70 @@ class CalcAddServer
 			e.printStackTrace();
 		}
 	}
+	private boolean connectSocket () {
+		try{
+			this.socket = server.accept();
+			System.out.println("연결 완료");
+			this.bw = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+			this.br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	private void receive () {
+		try {
+			this.exp = this.br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private int calcuration () {
+		String[] numbers = this.exp.split("\\+");
+		// 더하기 계산
+		int result = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		return result;
+	}
+
+	private void send () {
+		try {
+			int result = calcuration();
+			this.bw.write(result+"\n"); // 결과값 전송
+			this.bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	private void closed () {
+		try {
+			this.socket.close();
+			this.br.close();
+			this.bw.close();
+			System.out.println("연결 끊음");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 	public void process () {
 		while(true){
-			try{
 				System.out.println("더하기 연산 서버");
-				System.out.println("클라이언트와의 연결 기다리는 중..");
-
-				Socket socket = server.accept();
-				System.out.println("연결 완료");
-				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-				// 계산식을 받음
-				String exp = br.readLine();
-
-				String[] numbers = exp.split("\\+");
-
-				// 더하기 계산
-				int result = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
-				
-				// 결과 전송
-				bw.write(result+"\n");
-				bw.flush();
-				
-				// 연결 끊음.
-				br.close();
-				bw.close();
-				socket.close();
-				
-				System.out.println("연결 끊음");
-				
-			}catch(IOException e){
-				e.printStackTrace();
-			}
+				System.out.println("클라이언트와의 연결 기다리는 중..");	
+				if(!connectSocket()) {
+					System.out.println("소켓 연결 실패");
+					continue;
+				}
+				receive();
+				send();
+				closed();
 		}
 	}
 	public static void main(String[] args) 
